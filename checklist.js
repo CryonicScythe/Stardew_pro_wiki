75874e35a2fe812b0c518e9a3fa96c475dda1ea4
Alexander <113724373+CryonicScythe@users.noreply.github.com>
2026-02-11 14:09:30 -0800

    v1.1
    Attempting to add an on-click event

--- checklist.js
+++ checklist.js
@@ -3,3 +3,15 @@ When Toggled, list will:
     - Strikethrough
     - Save when refreshed
 */
+
+// Variables
+const strikeBox = document.getElementById("checkbox");
+
+
+// Strikethrough text on click
+strikeBox.addEventListener('click', toggleEvent)
+
+function toggleEvent() {
+    strikeBox.classList.toggle("striked")
+    strikeBox.innerHTML
+}

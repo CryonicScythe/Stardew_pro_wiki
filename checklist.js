33ac2334c9596d67ed92e50031043dda7e97406a
Alexander <113724373+CryonicScythe@users.noreply.github.com>
2026-02-26 13:07:18 -0800

    v1.5
    Added the collapse feature to each main section

--- checklist.js
+++ checklist.js
@@ -5,19 +5,41 @@ When Toggled, list will:
 */
 
 // Variables
-const strikeBox = document.getElementById("checkbox");
+const spring = document.getElementById("spring");
+const summer = document.getElementById("summer");
+const fall = document.getElementById("fall");
+const winter = document.getElementById("winter");
+const anySeason = document.getElementById("any");
+const springDropdown = document.getElementById("springBox");
+const summerDropdown = document.getElementById("summerBox");
+const fallDropdown = document.getElementById("fallBox");
+const winterDropdown = document.getElementById("winterBox");
+const anySeasonDropdown = document.getElementById("anyBox");
 
 
 // Strikethrough text on click
-strikeBox.addEventListener("click", toggleEvent)
+spring.addEventListener("click", toggleEvent1)
+summer.addEventListener("click", toggleEvent2)
+fall.addEventListener("click", toggleEvent3)
+winter.addEventListener("click", toggleEvent4)
+anySeason.addEventListener("click", toggleEvent5)
 
-function toggleEvent() {
-    strikeBox.classList.toggle("striked")
+function toggleEvent1() {
+    springDropdown.classList.toggle("hidden")
 }
 
+function toggleEvent2() {
+    summerDropdown.classList.toggle("hidden")
+}
 
-// Function in html code
-function change(id) {
-        check = document.getElementById(id)
-        check.classList.toggle("striked")
-    }
+function toggleEvent3() {
+    fallDropdown.classList.toggle("hidden")
+}
+
+function toggleEvent4() {
+    winterDropdown.classList.toggle("hidden")
+}
+
+function toggleEvent5() {
+    anySeasonDropdown.classList.toggle("hidden")
+}

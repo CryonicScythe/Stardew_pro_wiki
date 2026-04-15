21fdbbe48652c8083e4ccdda7c8d6ac0c4cd15d6
Alexander <113724373+CryonicScythe@users.noreply.github.com>
2026-04-15 14:08:47 -0700

    v4.1
    Began using PHP to create a gift calculator

---
+++ romance.php
@@ -0,0 +1,21 @@
+<?php
+
+$gifts = $_POST['gifts'];
+$quality = $_POST['quality'];
+
+
+# Gaining a value from "quality" submitted input
+if ($quality == "iridium") {
+
+}
+elseif ($quality == "gold") {
+
+}
+elseif ($quality == "silver"){
+
+}
+else ($quality == "basic") {
+
+}
+
+echo("<p>Welcome " . $gifts . " " . $quality . " </p");

25aa72474ddd16d94b461a2ce7dd62c02b918ee0
Alexander <113724373+CryonicScythe@users.noreply.github.com>
2026-04-17 13:45:58 -0700

    v4.1.1
    Finished calculator code, attempting to send php file data to romance.html

--- romance.php
+++ romance.php
@@ -2,20 +2,50 @@
 
 $gifts = $_POST['gifts'];
 $quality = $_POST['quality'];
+$gift_value = 0;
+$quality_value = 0.0;
+$friendship = 0.0;
+
+
+# Gaining a value from "gifts" submitted input
+if ($gifts == "loved") {
+    $gift_value = 80;
+}
+elseif ($gifts == "liked") {
+    $gift_value = 45;
+}
+elseif ($gifts == "neutral"){
+    $gift_value = 20;
+}
+elseif ($gifts == "disliked") {
+    $gift_value = -20;
+}
+elseif ($gifts == "hated") {
+    $gift_value = -40;
+}
 
 
 # Gaining a value from "quality" submitted input
 if ($quality == "iridium") {
-
+    $quality_value = 1.5;
 }
 elseif ($quality == "gold") {
-
+    $quality_value = 1.25;
 }
 elseif ($quality == "silver"){
-
+    $quality_value = 1.1;
 }
-else ($quality == "basic") {
-
+elseif ($quality == "basic") {
+    $quality_value = 1;
 }
 
-echo("<p>Welcome " . $gifts . " " . $quality . " </p");
+
+if ($gifts == "loved" or $gifts == "liked") {
+    $friendship = $quality_value * $gift_value;
+}
+else {
+    $friendship = $gift_value;
+}
+
+echo("Total friendship gained: " . $friendship)
+?>
